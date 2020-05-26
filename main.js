ec2be11549e994dadd4f9cd4b795e204e552408d
RicardoRaymundo <47898513+RicardoRaymundo@users.noreply.github.com>
2020-05-26 01:00:40 -0300

    test commit

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ var inputElementReply = document.getElementById("email-header15-a");
 var buttonSubmit = document.getElementById("submit-button");
 var MainContainerElement = document.getElementById("header16-3");
 
-
+alert("COMMIT");
 
 buttonSubmit.onclick = onSubmitQuery;
+
 
 function onSubmitQuery() {
     if (inputElementQuery.value == "" || inputElementReply == "") {
